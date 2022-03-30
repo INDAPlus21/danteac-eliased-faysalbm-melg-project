cabccdb05f013723a0f7d0317e130f94a3a0be41
MazeOfIth <76459089+MazeofIth@users.noreply.github.com>
2022-03-31 00:18:18 +0200

    create fun abstractions

--- main.js
+++ main.js
@@ -21,33 +21,37 @@ var white_height = white_width * 5.5
 var black_width = white_width - white_width / 3
 var black_height = white_height * 0.65
 
-var black_start_x = white_width - black_width / 2
-var white_start_x = 0
-var start_y = 0
+function drawInitialCanvas() {
+    var black_start_x = white_width - black_width / 2
+    var white_start_x = 0
+    var start_y = 0
 
-for (var i = 0; i < 100; i++) {
-    // fillRect() parameters x, y-coordinates of upper-left corner, width, height 
+    for (var i = 0; i < 100; i++) {
+        // fillRect() parameters x, y-coordinates of upper-left corner, width, height 
 
-    // draw white tiles 
-    ctx.strokeStyle = 'black';
-    for (var j = 0; j < 7; j++) {
-        ctx.rect(white_start_x + white_width * j, start_y, white_width, white_height);
+        // draw white tiles 
+        ctx.strokeStyle = 'black';
+        for (var j = 0; j < 7; j++) {
+            ctx.rect(white_start_x + white_width * j, start_y, white_width, white_height);
+        }
+        ctx.stroke();
+
+        ctx.fillStyle = "black"
+        // var tile_width = 150
+        for (var j = 0; j < 3; j++) {
+            ctx.fillRect(black_start_x + white_width * j, start_y, black_width, black_height);
+        }
+        var distance = black_start_x + white_width * j + white_width
+        for (var j = 0; j < 2; j++) {
+            ctx.fillRect(distance + white_width * j, start_y, black_width, black_height);
+        }
+
+        black_start_x += 7 * white_width
+        white_start_x += 7 * white_width
     }
-    ctx.stroke();
-
-    ctx.fillStyle = "black"
-    // var tile_width = 150
-    for (var j = 0; j < 3; j++) {
-        ctx.fillRect(black_start_x + white_width * j, start_y, black_width, black_height);
-    }
-    var distance = black_start_x + white_width * j + white_width
-    for (var j = 0; j < 2; j++) {
-        ctx.fillRect(distance + white_width * j, start_y, black_width, black_height);
-    }
-
-    black_start_x += 7 * white_width
-    white_start_x += 7 * white_width
 }
+
+drawInitialCanvas()
 
 function playSound(url) {
     const audio = new Audio(url);
@@ -67,48 +71,107 @@ function printMousePos(event, type) {
     // var g = data[index + 1] these aren't used either, bc binary
     // var b = data[index + 2]
 
-    if (x < white_width && r == 255) {
-        playSound("./piano-mp3/F2.mp3")
-    } else if (x < white_width * 2 && x > white_width && r == 255) {
-        playSound("./piano-mp3/G2.mp3")
-    } else if (x < white_width * 3 && x > white_width * 2 && r == 255) {
-        playSound("./piano-mp3/A2.mp3")
-    } else if (x < white_width * 4 && x > white_width * 3 && r == 255) {
-        playSound("./piano-mp3/B2.mp3")
-    } else if (x < white_width * 5 && x > white_width * 4 && r == 255) {
-        playSound("./piano-mp3/C3.mp3")
-    } else if (x < white_width * 6 && x > white_width * 5 && r == 255) {
-        playSound("./piano-mp3/D3.mp3")
-    } else if (x < white_width * 7 && x > white_width * 6 && r == 255) {
-        playSound("./piano-mp3/E3.mp3")
+    var white_start_x = 0
+    var black_start_x = white_width - black_width / 2
+    var key_order = ["F", "G", "A", "B", "C", "D", "E"]
+    var flats_key_order = ["G", "A", "B", "D", "E"]
+    var octave = 1
+    for (var i = 1; i < 4; i++) {
+        for (var j = 0; j < key_order.length; j++) {
+            if (key_order[j] == "C") {
+                octave++
+            }
+            if (x > white_start_x + white_width * j && x < white_start_x + white_width * (j + 1) && r == 255) {
+                playSound("./piano-mp3/" + key_order[j] + (octave + i) + ".mp3")
+            }
+        }
+
+        octave = 1
+        key_index = 0 
+        for (var j = 0; j < flats_key_order.length+1; j++) {
+            /* if (flats_key_order[j] == "D") {
+                octave++
+            } */
+            if (j == 3) {
+                j++
+                octave++
+                // key_index--
+            }
+            console.log({key_index}, {j}, {black_start_x}, {white_width})
+            if (x > black_start_x + white_width * j && x < black_start_x + white_width * j + black_width && r == 0) {
+                console.log("./piano-mp3/" + flats_key_order[key_index] + "b" + (octave + i) + ".mp3")
+                playSound("./piano-mp3/" + flats_key_order[key_index] + "b" + (octave + i) + ".mp3")
+            } 
+            key_index++
+        }
+        octave = 1
+        /* octave++
+
+        for (var j = 4; j < 6; j++) {
+            // console.log({j}, flats_key_order)
+            if (x > black_start_x + white_width * j && x < black_start_x + white_width * j + black_width && r == 0) {
+                console.log("./piano-mp3/" + flats_key_order[j-1] + "b" + (octave + i) + ".mp3")
+                playSound("./piano-mp3/" + flats_key_order[j-1] + "b" + (octave + i) + ".mp3")
+            } 
+        } */
+
+        /* if (x > black_start_x && x < black_start_x + black_width && r == 0) {
+            playSound("./piano-mp3/Gb" + (1 + i) + ".mp3")
+        } else if (x > black_start_x + white_width && x < black_start_x + white_width + black_width && r == 0) {
+            playSound("./piano-mp3/Ab" + (1 + i) + ".mp3")
+        } else if (x > black_start_x + white_width * 2 && x < black_start_x + white_width * 2 + black_width && r == 0) {
+            playSound("./piano-mp3/Bb" + (1 + i) + ".mp3")
+        } else if (x > black_start_x + white_width * 4 && x < black_start_x + white_width * 4 + black_width && r == 0) {
+            playSound("./piano-mp3/Db" + (2 + i) + ".mp3")
+        } else if (x > black_start_x + white_width * 5 && x < black_start_x + white_width * 5 + black_width && r == 0) {
+            playSound("./piano-mp3/Eb" + (2 + i) + ".mp3")
+        } */
+        black_start_x += 7 * white_width
+        white_start_x += 7 * white_width
     }
+    /* if (x > white_start_x && x < white_start_x + white_width && r == 255) {
+        playSound("./piano-mp3/" + keyorder[i-1] + (1 + i) + ".mp3")
+    } else if (x < white_width * 2 + white_start_x && x > white_start_x + white_width && r == 255) {
+        console.log("in g", 1 + i)
+        playSound("./piano-mp3/G" + (1 + i) + ".mp3")
+    } else if (x < white_width * 3 + white_start_x && x > white_start_x + white_width * 2 && r == 255) {
+        console.log("in A", 1 + i)
+        playSound("./piano-mp3/A" + (1 + i) + ".mp3")
+    } else if (x < white_width * 4 + white_start_x && x > white_start_x + white_width * 3 && r == 255) {
+        playSound("./piano-mp3/B" + (1 + i) + ".mp3")
+    } else if (x < white_width * 5 + white_start_x && x > white_start_x + white_width * 4 && r == 255) {
+        playSound("./piano-mp3/C" + (2 + i) + ".mp3")
+    } else if (x < white_width * 6 + white_start_x && x > white_start_x + white_width * 5 && r == 255) {
+        playSound("./piano-mp3/D" + (2 + i) + ".mp3")
+    } else if (x < white_width * 7 + white_start_x && x > white_start_x + white_width * 6 && r == 255) {
+        playSound("./piano-mp3/E" + (2 + i) + ".mp3")
+    } */
 
-    black_start_x = white_width - black_width / 2
     // console.log({x}, {black_start_x}, {black_width}) 
-    if (x > black_start_x && x < black_start_x + black_width && r == 0) {
-        playSound("./piano-mp3/Gb2.mp3")
-    } else if (x > black_start_x + white_width && x < black_start_x + white_width + black_width && r == 0) {
-        playSound("./piano-mp3/Ab2.mp3")
-    } else if (x > black_start_x + white_width * 2 && x < black_start_x + white_width * 2 + black_width && r == 0) {
-        playSound("./piano-mp3/Bb2.mp3")
-    } else if (x > black_start_x + white_width * 4 && x < black_start_x + white_width * 4 + black_width && r == 0) {
-        playSound("./piano-mp3/Db3.mp3")
-    } else if (x > black_start_x + white_width * 5 && x < black_start_x + white_width * 5 + black_width && r == 0) {
-        playSound("./piano-mp3/Eb3.mp3")
-    } 
-
-    // var a = data[index + 3] // alpha, how opaque pixels are. isn't used
-
-    // 255, 255, 255 is white. 0, 0, 0 is black 
-    // console.log({ index }, { r })
-
-    // y = Math.floor(y / cell_size)
-    // x = Math.floor(x / cell_size)
-    // console.log(x, y)
-    // console.log(x, y)
 }
 
+// var a = data[index + 3] // alpha, how opaque pixels are. isn't used
+
+// 255, 255, 255 is white. 0, 0, 0 is black 
+// console.log({ index }, { r })
+
+// y = Math.floor(y / cell_size)
+// x = Math.floor(x / cell_size)
+// console.log(x, y)
+// console.log(x, y)
+
 canvas.addEventListener("click", printMousePos, true)
+
+/* canvas.onmousemove = function (event) {
+    var white_start_x = 0
+    var start_y = 0
+    console.log("hi")
+    x = event.clientX
+    y = event.clientY
+    if
+    ctx.fillStyle = 'grey';
+    ctx.fillRect(white_start_x + white_width * 0, start_y, white_width, white_height);
+} */
 
 
 // printMousePos()
