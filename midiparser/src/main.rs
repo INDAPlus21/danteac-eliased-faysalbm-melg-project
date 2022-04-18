8c076e41b29db9b2f8642808a66197190a48b0e4
Marcus Elg <melg@kth.se>
2022-04-18 10:32:46 +0200

    Parse track data

--- midiparser/src/main.rs
+++ midiparser/src/main.rs
@@ -1,6 +1,5 @@
 use core::convert::TryInto;
 use std::fs;
-use std::io::{BufRead, BufReader};
 mod song;
 use song::{Song, Track};
 
@@ -13,7 +12,6 @@ fn main() {
 fn parse_midi(filename: &str) -> Option<Song> {
     if let Ok(data) = fs::read(filename.to_owned() + ".mid") {
         let mut tracks = vec![];
-        println!("{:?}", data);
 
         let mut i = 0;
         while i < data.len() {
@@ -30,7 +28,7 @@ fn parse_midi(filename: &str) -> Option<Song> {
                 //let format = data[i]; // 0: Single track 1: Parallel tracks 2: Sequental tracks
                 i += 1;
 
-                // Chunk count
+                // Chunk count (will always be 6)
                 //let chunk_count = data[i];
                 i += 1;
 
@@ -40,11 +38,66 @@ fn parse_midi(filename: &str) -> Option<Song> {
             } else if i < data.len() - 4 && data[i..i + 4] == b"MTrk".to_owned() {
                 // Parse track (only note on and off events)
                 // Expects songs with format 1 (parallel)
-                println!("TRACK!");
                 i += 4;
+
                 let mut notes = vec![];
                 let mut volumes = vec![];
                 let mut offsets = vec![];
+
+                // Byte count
+                let byte_count = bytes_to_int(&data[i..i + 4]);
+                i += 4;
+                let start_i = i;
+
+                // Read track events (doesn't account for SysEx messages)
+                while i - start_i < byte_count as usize {
+                    let delta = variable_length_bytes_to_int(&data, &mut i);
+
+                    // Status byte (type and channel)
+                    let event_type = data[i] >> 4; // Top part of byte
+                                                   //let channel = (data[i] << 4) >> 4; // Bottom part of byte
+                    i += 1;
+
+                    match event_type {
+                        8 | 9 => {
+                            // Note on/off event
+                            offsets.push(delta as f32);
+                            notes.push(data[i] as f32);
+
+                            // Note off should always have volume 0
+                            if event_type == 9 {
+                                volumes.push(0f32);
+                            } else {
+                                volumes.push(data[i + 1] as f32);
+                            }
+
+                            i += 2;
+                        }
+                        10 | 11 | 14 => {
+                            // Skip event
+                            i += 2;
+                        }
+                        12 | 13 => {
+                            // Skip event
+                            i += 1;
+                        }
+                        _ => {}
+                    }
+                    if data[i - 1] == 255 {
+                        // Meta events
+                        let message_type = data[i];
+                        i += 1;
+
+                        let length = data[i];
+                        i += (1 + length) as usize;
+
+                        // End of track, ignore other messages
+                        if message_type == 47 {
+                            break;
+                        }
+                    }
+                }
+
                 // Only add tracks with note data
                 if notes.len() > 0 {
                     tracks.push(Track {
@@ -63,6 +116,28 @@ fn parse_midi(filename: &str) -> Option<Song> {
         println!("Error: File {}.mid not found!", filename);
         None
     }
+}
+
+// Source: https://stackoverflow.com/questions/24711585/decode-midi-variable-length-field
+// Returns bytes as integer
+fn variable_length_bytes_to_int(data: &[u8], index: &mut usize) -> u32 {
+    let mut ret = 0;
+
+    loop {
+        let byte_in = data[*index];
+        *index += 1;
+        if byte_in == 0 {
+            return 0;
+        }
+
+        // Continue if top bit is one
+        ret = (ret << 7) | (byte_in & 0x7f);
+        if (byte_in & 0x80) == 0 {
+            return ret as u32;
+        }
+    }
+
+    0
 }
 
 fn bytes_to_int(bytes: &[u8]) -> u32 {
