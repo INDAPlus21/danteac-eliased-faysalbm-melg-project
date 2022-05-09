d6414a3a0d749b55862a84cb679653564fb188e4
MazeOfIth <76459089+MazeofIth@users.noreply.github.com>
2022-05-09 17:35:17 +0200

    try to make parser work

--- midiparser/src/main.rs
+++ midiparser/src/main.rs
@@ -2,8 +2,7 @@ use midiparser::parse_midi;
 use midiparser::convert_to_front_end_format;
 
 fn main() {
-    println!("hello world");
-    let parsed = parse_midi("musemario");
+    let parsed = parse_midi("sax_medley");
     println!("{:?}", parsed);  
     println!("{:?}", convert_to_front_end_format(parsed.unwrap()));
 }
