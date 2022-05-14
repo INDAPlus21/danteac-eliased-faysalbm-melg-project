95b6f566ce1f77858e2269b9afb30b980b413b07
Dante Astorga Castillo <danteac@kth.se>
2022-05-14 17:58:31 +0200

    Add flatten functionality to Matrix struct.

--- sentence-predicting-ai/many-to-one/src/linalg/matrix.rs
+++ sentence-predicting-ai/many-to-one/src/linalg/matrix.rs
@@ -115,6 +115,20 @@ impl Matrix {
         }
         output
     }
+
+    // Concatenates all row vectors into one column vector.
+    pub fn flatten(&self) -> Vector {
+        let self_height: usize = self.get_height();
+        let self_width: usize = self.get_width();
+        let output_len: usize = self_height * self_width;
+        let mut output: Vector = Vector::with_length(output_len);
+        for row in 0..self_height {
+            for col in 0..self_width {
+                output[row * self_width + col] = self[row][col];
+            }
+        }
+        output
+    }
     //endregion
 }
 
